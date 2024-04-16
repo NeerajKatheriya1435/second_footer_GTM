36bb73b6715ffb2dcec0bf84b27feda30768446f
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2024-04-16 12:48:55 +0530

    fix font size and background css

--- src/footer/FormFooter.js
+++ src/footer/FormFooter.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import "./FormFooter.css"
+import styles from "./FormFooter.module.css"
 import gtmLogo from "../img/gtmLogo.png"
 import axios from 'axios'
 const FormFooter = () => {
@@ -29,45 +29,45 @@ const FormFooter = () => {
     return (
         <>
             <main>
-                <div className="mainContainer">
-                    <div className="myBox">
-                        <div className="imgBox">
+                <div className={styles.mainContainer}>
+                    <div className={styles.myBox}>
+                        <div className={styles.imgBox}>
                             <img src={gtmLogo} alt="" />
                         </div>
-                        <h1 className='unpaidHeading iconAndText'>Unpaid Professionals</h1>
-                        <div className="iconAndText">
+                        <h1 className={`${styles.unpaidHeading} ${styles.iconAndText}`}>Unpaid Professionals</h1>
+                        <div className={styles.iconAndText}>
                             <p>Hii We're UI/UX Designers</p>
                         </div>
-                        <div className="iconAndText">
+                        <div className={styles.iconAndText}>
                             <img src="https://png.pngtree.com/png-vector/20191021/ourmid/pngtree-vector-location-icon-png-image_1834331.jpg" alt="" />
                             <p>6969 Gali, Mohalla 420 Nala Sopara</p>
                         </div>
-                        <div className="iconAndText">
+                        <div className={styles.iconAndText}>
                             <img src="https://png.pngtree.com/png-vector/20190927/ourmid/pngtree-email-icon-png-image_1757854.jpg" alt="" />
                             <p>Hit&runoverme@200speed.com</p>
                         </div>
-                        <div className="iconAndText">
+                        <div className={styles.iconAndText}>
                             <img src="https://w7.pngwing.com/pngs/421/683/png-transparent-computer-icons-mobile-phones-telephone-email-home-business-phones-phone-icon-miscellaneous-angle-service-thumbnail.png" alt="" />
                             <p>(91+) 6996878896</p>
                         </div>
                     </div>
-                    <form onSubmit={handleSubmit} className="myBox">
-                        <h1 className='contactHeading'>Contact Us</h1>
-                        <input onChange={handleChange} value={formData.name} className='inputForm' name='name' type="text" placeholder='Name' />
-                        <input onChange={handleChange} value={formData.number} className='inputForm' name='number' type="number" placeholder='Number' />
-                        <input onChange={handleChange} value={formData.email} className='inputForm' name='email' type="email" placeholder='Email' />
-                        <input onChange={handleChange} value={formData.message} className='inputForm inputTextArea' name='message' type="text" placeholder='Message' />
-                        <button className='inputForm' type='submit'>Submit</button>
+                    <form onSubmit={handleSubmit} className={styles.myBox}>
+                        <h1 className={styles.contactHeading}>Contact Us</h1>
+                        <input onChange={handleChange} value={formData.name} className={styles.inputForm} name='name' type="text" placeholder='Name' />
+                        <input onChange={handleChange} value={formData.number} className={styles.inputForm} name='number' type="number" placeholder='Number' />
+                        <input onChange={handleChange} value={formData.email} className={styles.inputForm} name='email' type="email" placeholder='Email' />
+                        <input onChange={handleChange} value={formData.message} className={`${styles.inputForm} ${styles.inputTextArea}`} name='message' type="text" placeholder='Message' />
+                        <button className={styles.inputForm} type='submit'>Submit</button>
                     </form>
-                    <div className="myBox">
-                        <div className="footerPages">
-                            <h1 className='companyHeading'>Company</h1>
+                    <div className={styles.myBox}>
+                        <div className={styles.footerPages}>
+                            <h1 className={styles.companyHeading}>Company</h1>
                             <a href="/">About Us</a>
                             <a href="/">Careers</a>
                             <a href="/">FAQs</a>
                             <a href="/">Contact Us</a>
                         </div>
-                        <div className="imgLogoFooter">
+                        <div className={styles.imgLogoFooter}>
                             <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png" alt="" />
                             <img src="https://seeklogo.com/images/T/twitter-2012-negative-logo-5C6C1F1521-seeklogo.com.png" alt="" />
                             <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png" alt="" />
